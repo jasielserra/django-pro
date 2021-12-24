b69444fac6b02cd4159562db33af514c69820ec7
jasiel Serra <jasiel_serra@hotmail.com>
2021-12-24 16:27:43 -0300

    Implemtando test dos asserts dos formulários html.

--- webdev/tarefas/tests/test_tarefas_get.py
+++ webdev/tarefas/tests/test_tarefas_get.py
@@ -1,6 +1,11 @@
 from django.test import Client
 from django.urls import reverse
+from pytest_django.asserts import assertContains
 
 def test_status_code(client: Client):
     resposta = client.get(reverse('tarefas:home'))
     assert resposta.status_code == 200
+
+def test_formulario_present(client):
+    resposta = client.get(reverse('tarefas:home'))
+    assertContains(resposta, '<form')
