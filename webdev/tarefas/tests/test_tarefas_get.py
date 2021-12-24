48818f38c23db8fab1f8594b658757c3af75150f
jasiel Serra <jasiel_serra@hotmail.com>
2021-12-24 15:50:04 -0300

    Tests get da minha app Tarefas passando.

--- webdev/tarefas/tests/test_tarefas_get.py
+++ webdev/tarefas/tests/test_tarefas_get.py
@@ -1,5 +1,6 @@
+from django.test import Client
 from django.urls import reverse
 
-def test_status_code(client):
-    resposta = client.get(reverse('tarefas: home'))
+def test_status_code(client: Client):
+    resposta = client.get(reverse('tarefas:home'))
     assert resposta.status_code == 200
--- webdev/tarefas/urls.py
+++ webdev/tarefas/urls.py
@@ -1,9 +1,9 @@
-from django.urls import path, include
+from django.urls import path
 
 from webdev.tarefas import views
 
 app_name = 'tarefas'
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
 ]
