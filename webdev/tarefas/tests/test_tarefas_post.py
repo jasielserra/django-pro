78d03349ba7b5aeb5549aa7346f9da682c91e5a9
jasiel Serra <jasiel_serra@hotmail.com>
2021-12-24 19:26:07 -0300

    Realizada migração do modelo Tarefa

--- webdev/tarefas/tests/test_tarefas_post.py
+++ webdev/tarefas/tests/test_tarefas_post.py
@@ -3,9 +3,14 @@ from django.urls import reverse
 from webdev.tarefas.models import Tarefa
 
 @pytest.fixture
-def resposta(client):
+def resposta(client, db):
     resp = client.post(reverse('tarefas:home'), data={'nome':'Tarefa'})
     return resp
 
 def test_tarefa_existe_no_bd(resposta):
     assert Tarefa.objects.exists()
+
+@pytest.fixture
+def resposta_dado_invalido(client, db):
+    resp = client.post(reverse('tarefas:home'), data={'nome':''})
+    return resp
--- webdev/tarefas/views.py
+++ webdev/tarefas/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from webdev.forms import TarefaNovaForm
 # Create your views here.
+
+
+
 def home(request):
+    if request.method == 'POST':
+        form = TarefaNovaForm(request.POST)
+        if form.is_valid():
+            form.save()
     return render(request, 'tarefas/home.html')
