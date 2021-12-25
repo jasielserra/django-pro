8f33185e936405ce69019bd77dabbae4efd567a3
jasiel Serra <jasiel_serra@hotmail.com>
2021-12-25 14:43:53 -0300

    Implementada view que lista todas as tarefas.

--- webdev/tarefas/views.py
+++ webdev/tarefas/views.py
@@ -4,7 +4,7 @@ from webdev.forms import TarefaNovaForm
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 # Create your views here.
-
+from webdev.tarefas.models import Tarefa
 
 
 def home(request):
@@ -14,5 +14,7 @@ def home(request):
             form.save()
             return HttpResponseRedirect(reverse('tarefas:home'))
         else:
-            return render(request, 'tarefas/home.html', {'form': form}, status=400)
-    return render(request, 'tarefas/home.html')
+            tarefas_pendentes = Tarefa.objects.filter(feita=False).all()
+            return render(request, 'tarefas/home.html', {'form': form, 'tarefas_pendentes': tarefas_pendentes}, status=400)
+    tarefas_pendentes = Tarefa.objects.filter(feita=False).all()
+    return render(request, 'tarefas/home.html',{'tarefas_pendentes': tarefas_pendentes})
