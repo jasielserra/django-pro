27e53353b4f09c706e462d250eea61c3eab379af
jasiel Serra <jasiel_serra@hotmail.com>
2021-12-26 22:54:47 -0300

    Consertada fixture tarefa no modulos tests apagar tarefas

--- webdev/tarefas/tests/test_apagar_tarefa.py
+++ webdev/tarefas/tests/test_apagar_tarefa.py
@@ -3,7 +3,7 @@ from django.urls import reverse
 from webdev.tarefas.models import Tarefa
 
 @pytest.fixture
-def tarefa_pendente(db):
+def tarefa(db):
     return Tarefa.objects.create(nome='Tarefa 1', feita=True)
 
 @pytest.fixture
