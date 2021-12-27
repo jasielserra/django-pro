2df0469c569989c117bc2dc6a230471ee339b835
jasiel Serra <jasiel_serra@hotmail.com>
2021-12-26 22:58:33 -0300

    Refatorada função resposta no meu modulo de tests apagar tarefas.

--- webdev/tarefas/tests/test_apagar_tarefa.py
+++ webdev/tarefas/tests/test_apagar_tarefa.py
@@ -7,7 +7,7 @@ def tarefa(db):
     return Tarefa.objects.create(nome='Tarefa 1', feita=True)
 
 @pytest.fixture
-def resposta_com_tarefa(client, tarefa):
+def resposta(client, tarefa):
     resp = client.post(reverse('tarefas:apagar', kwargs ={'tarefa_id': tarefa.id}),
                        )
     return resp
